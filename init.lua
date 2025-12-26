e2ce265b5b51f776133e98130f40c4838f129aad
Maximilian Lallathin <zenfale@web.de>
2025-12-26 17:17:47 +0100

    Add python formatter and LSP

--- init.lua
+++ init.lua
@@ -678,7 +678,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        -- pyright = {},
+        -- Python formatter and linter (internet says its the fastest)
+        ruff = {},
+        -- Python LSP for autocompletion etc (chosen over pyright because pyright requires npm setup)
+        jedi_language_server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
