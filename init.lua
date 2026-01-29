7b98f662ad1f8881ad5f07a8814e60867ac79128
Maximilian Lallathin <maximilian.lallathin@mvtec.com>
2026-01-29 14:02:57 +0100

    Remove git state and use relative file path
    because git worktree with default Jira branch names results in very
    long paths

--- init.lua
+++ init.lua
@@ -459,6 +459,13 @@ do
   ---@diagnostic disable-next-line: duplicate-set-field
   statusline.section_location = function() return '%2l:%-2v' end
 
+  -- disable git information to save space
+  ---@diagnostic disable-next-line: duplicate-set-field
+  statusline.section_git = function() return '' end
+  -- show only relative file name
+  ---@diagnostic disable-next-line: duplicate-set-field
+  statusline.section_filename = function() return '%f' end
+
   -- ... and there is more!
   --  Check out: https://github.com/nvim-mini/mini.nvim
 end
