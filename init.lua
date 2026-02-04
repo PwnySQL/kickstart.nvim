6c741de3ffdf8c75e92ba89282da901f57ad7eb1
Maximilian Lallathin <maximilian.lallathin@mvtec.com>
2026-02-04 19:42:36 +0100

    add keybindings for luasnip
    insprired by TakeTuesday by TeeJ

--- init.lua
+++ init.lua
@@ -842,6 +842,23 @@ require('lazy').setup({
           --   end,
           -- },
         },
+        config = function()
+          local ls = require 'luasnip'
+          -- expand or jump into snippet
+          vim.keymap.set({ 'i' }, '<C-K>', function()
+            ls.expand_or_jump()
+          end, { silent = true })
+          -- go back in snippet, J as the opposite of K
+          vim.keymap.set({ 'i' }, '<C-J>', function()
+            ls.jump(-1)
+          end, { silent = true })
+          -- select between choices in a snippet, L for list
+          vim.keymap.set({ 'i', 's' }, '<C-L>', function()
+            if ls.choice_active() then
+              ls.change_choice(1)
+            end
+          end, { silent = true })
+        end,
         opts = {},
       },
       'folke/lazydev.nvim',
