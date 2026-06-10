47cb5a13b0368b2be84eef68cb469a26b1397f51
Maximilian Lallathin <maximilian.lallathin@mvtec.com>
2026-06-10 17:36:46 +0200

    Enable nixfmt on file save

--- init.lua
+++ init.lua
@@ -792,6 +792,7 @@ do
       local enabled_filetypes = {
         -- lua = true,
         -- python = true,
+        nix = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
         return { timeout_ms = 500 }
@@ -807,6 +808,7 @@ do
       -- rust = { 'rustfmt' },
       lua = { 'stylua' },
       go = { 'gofumpt' },
+      nix = { 'nixfmt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
